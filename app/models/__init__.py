2c2bc6ca5b6f7b2afc0ec7500fa9b066736a2cd0
Cheryn Shin <cherynjjaeshin@gmail.com>
2023-09-18 23:20:05 -0700

    order toppings not order topping

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from .db import db
-from .models import User, Order, Drink, Topping, Review, order_topping
+from .models import User, Order, Drink, Topping, Review, order_toppings
 from .db import environment, SCHEMA
 from .db import db
